107981d8664c07fe21039c3b6c75adfc14d293ba
noraghastudio <king.need4@gmail.com>
2026-06-21 14:38:21 +0300

    Cleanup: remove backend and utility scripts as they are no longer needed (migrated to Appwrite)

--- test_fetch.js
+++
@@ -1,9 +0,0 @@
-const { Client, Databases, Query } = require('node-appwrite');
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('6a36c593002500c98660');
-const databases = new Databases(client);
-
-databases.listDocuments('6a36cb6c0009fc808a1f', 'reviews', [Query.limit(5)])
-    .then(res => console.log('Success. Found:', res.documents.length))
-    .catch(err => console.error('Error:', err));
